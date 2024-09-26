f4e531a29fef285f466317aade0e85447e535080
hillary1343 <lindsayhillary8@gmail.com>
2024-09-26 18:18:27 -0300

    inicio version

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿String Usuario;
+String traducao;
+
+Console.Clear();
+
+
+Console.WriteLine("traduzindo para baleiês");
+Console.WriteLine("Escreva seu nome.");
+
+Usuario = Console.ReadLine()!;
+
+
+traducao = Usuario.Replace("a", "aaa")
+                .Replace("A", "AAA")
+                .Replace("E", "EEE")
+                .Replace("e", "eee")
+                .Replace("I", "III")
+                .Replace("i", "iii")
+                .Replace("O", "OOO")
+                .Replace("o", "ooo")
+                .Replace("u", "uuu")
+                .Replace("U", "UUU");
+                
+
+Console.WriteLine($"Seu nome em baleiês fica assim {traducao}.");
